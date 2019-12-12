87646dd44580b5be67a4731f565a23fa7d3a6234
Felipe Padilha <felipe.padilha@gft.com>
2019-12-12 09:10:27 -0300

    Removed ML option and updated cameraX libs

---
+++ app/src/main/java/github/hotstu/camerax/qrcodec/App.kt
@@ -0,0 +1,11 @@
+package github.hotstu.camerax.qrcodec
+
+import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class App : Application(), CameraXConfig.Provider {
+    override fun getCameraXConfig(): CameraXConfig {
+        return Camera2Config.defaultConfig(this)
+    }
+}
--- app/src/main/java/github/hotstu/camerax/qrcodec/MainActivity.kt
+++ app/src/main/java/github/hotstu/camerax/qrcodec/MainActivity.kt
@@ -4,27 +4,25 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
-import android.util.DisplayMetrics
 import android.util.Log
-import android.util.Rational
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraX
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageAnalysisConfig
-import androidx.camera.core.PreviewConfig
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.tbruyelle.rxpermissions2.RxPermissions
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executor
 
 const val FLAGS_FULLSCREEN =
-        View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    View.SYSTEM_UI_FLAG_LOW_PROFILE or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 
 const val IMMERSIVE_FLAG_TIMEOUT = 500L
 
@@ -32,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     var analyzerHandler: Handler? = null
     var analysis: ImageAnalysis? = null
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,60 +39,67 @@ class MainActivity : AppCompatActivity() {
 
         val rxPermissions = RxPermissions(this)
         rxPermissions.request(android.Manifest.permission.CAMERA)
-                .subscribe {
-                    textureView.post {
-                        val metrics = DisplayMetrics().also { textureView.display.getRealMetrics(it) }
-                        val screenAspectRatio = Rational(metrics.widthPixels, metrics.heightPixels)
+            .subscribe {
+                previewView.post {
 
-                        val previewConfig = PreviewConfig.Builder().apply {
-                            setTargetAspectRatio(screenAspectRatio)
-                            setTargetRotation(textureView.display.rotation)
-                            setLensFacing(CameraX.LensFacing.BACK)
-                        }.build()
+                    cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-                        val analysisConfig = ImageAnalysisConfig.Builder().apply {
-                            setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
-                            val analyzerThread = HandlerThread("BarcodeAnalyzer").apply { start() }
-                            analyzerHandler = Handler(analyzerThread.looper)
-                            setCallbackHandler(analyzerHandler!!)
-                            setTargetAspectRatio(screenAspectRatio)
-                            setTargetRotation(textureView.display.rotation)
-                            setLensFacing(CameraX.LensFacing.BACK)
-                        }.build()
+                    val cameraSelector = CameraSelector.Builder()
+                        .requireLensFacing(LensFacing.BACK).build()
 
-                        val preview = AutoFitPreviewBuilder.build(previewConfig, textureView)
-                        analysis = ImageAnalysis(analysisConfig)
+                    val preview = Preview.Builder()
+                        .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                        .setTargetRotation(previewView.display.rotation)
+                        .build()
+                    preview.previewSurfaceProvider = previewView.previewSurfaceProvider
 
-                        val googlePlayServicesAvailable = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
-                        if (googlePlayServicesAvailable == ConnectionResult.SUCCESS) {
-                            Log.d("MainActivity", "google play services avalable, using visionBarcodeDetector")
-                            analysis!!.analyzer = MLQRcodeAnalyzer()
-                        } else {
-                            Log.d("MainActivity", "google play services inavalable, fallback to zxing")
-                            analysis!!.analyzer = QRcodeAnalyzer()
-                        }
+                    analysis = ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.BackpressureStrategy.KEEP_ONLY_LATEST)
+                        .build()
 
-                        CameraX.bindToLifecycle(this@MainActivity,
-                                preview,
-                                analysis)
+                    val analyzerThread = HandlerThread("BarcodeAnalyzer").apply { start() }
+                    analyzerHandler = Handler(analyzerThread.looper)
 
-                    }
+                    val han = analyzerHandler!!
+                    analysis!!.setAnalyzer(
+                        Executor { han.post(it) },
+                        ZxingQrCodeAnalyzer {
+                            Log.d("QR", "Result: $it")
+                            Toast.makeText(this@MainActivity, it.text, Toast.LENGTH_SHORT).show()
+                        })
+
+                    cameraProviderFuture.addListener(Runnable {
+                        val cameraProvider = cameraProviderFuture.get()
+                        cameraProvider.bindToLifecycle(
+                            this@MainActivity,
+                            cameraSelector,
+                            preview,
+                            analysis
+                        )
+                    }, ContextCompat.getMainExecutor(this))
+//                    CameraX.bindToLifecycle(
+//                        this@MainActivity,
+//                        preview,
+//                        analysis
+//                    )
+
                 }
+            }
     }
 
     override fun onResume() {
         super.onResume()
         // Before setting full screen flags, we must wait a bit to let UI settle; otherwise, we may
         // be trying to set app to immersive mode before it's ready and the flags do not stick
-        textureView.postDelayed({
-            textureView.systemUiVisibility = FLAGS_FULLSCREEN
+        previewView.postDelayed({
+            previewView.systemUiVisibility = FLAGS_FULLSCREEN
         }, IMMERSIVE_FLAG_TIMEOUT)
     }
 
     override fun onDestroy() {
         analyzerHandler?.removeCallbacksAndMessages(null)
         analyzerHandler?.looper?.quitSafely()
-        analysis?.analyzer = null
+        analysis?.clearAnalyzer()
         super.onDestroy()
     }
 }
